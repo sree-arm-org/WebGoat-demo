a574a8c2b4c78bef14a17521586085dfe4e05405
sreejith-armorcode <sreejith.menon@armorcode.io>
2025-02-13 18:30:44 +0530

    Create secrets.py

---
+++ src/main/resources/secrets.py
@@ -0,0 +1,16 @@
+# Example in Python
+API_KEY = "API_KEY_PLACEHOLDER_abcdef1234567890"
+password = "PASSWORD_PLACEHOLDER_MyS3cr3tP4sswOrd"
+
+def my_function():
+    # Some code...
+    db_password = "DATABASE_PASSWORD_PLACEHOLDER_qwertyuiop"
+    # ... more code
+
+ssh_key = """
+-----BEGIN RSA PRIVATE KEY-----
+FAKE_SSH_KEY_PLACEHOLDER_RANDOM_CHARACTERS
+-----END RSA PRIVATE KEY-----
+"""
+
+print(f"Base64: {BASE64_ENCODED_PLACEHOLDER_DATA}") # Example of a f-string
